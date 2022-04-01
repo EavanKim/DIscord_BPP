e5277d743aad6ce5b52f5b56b17d7de5c9252410
eavankim <kjtkjt21@gmail.com>
2022-04-01 15:24:18 +0900

    [수정] 경로 재설정 및 빌드 확인
    빌드 정상진행 화인 후 커밋.

--- Discord_BPP/Discord/Discord_Include.h
+++ Discord_BPP/Discord/Discord_Include.h
@@ -5,22 +5,19 @@
 #include <unordered_map>
 #include <stdint.h>
 
-
-#include <windows.h>
 #include <WinSock2.h>
-#include <ws2tcpip.h>
+#include <Windows.h>
+#include <process.h>
 
 // Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
-#pragma comment (lib, "AdvApi32.lib")
 
 #include "Discord_Defines.h"
 #include "Discord_Exception.h"
 #include "Discord_Context.h"
-#include "Discord_Work.h"
-#include "Discord_Task.h"
-#include "Discord_Socket.h"
-#include "Discord_Bot_Server.h"
+#include "Thread/Discord_Work.h"
+#include "Thread/Discord_Task.h"
+#include "Server/Discord_Socket.h"
+#include "Server/Discord_Bot_Server.h"
 
 #endif
